94898390cb766200ac1a30519d332d5e9a4a09ca
Egor Pankov <62761016+egorpankov@users.noreply.github.com>
2021-03-15 23:47:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+let swithMode = document.getElementById("swithMode");
+swithMode.onclick = function () {
+    let theme = document.getElementById("theme")
+
+    if (theme.getAttribute("href") == "style.css") {
+        theme.href = "dark-mode.css"
+    } else {
+        theme.href = "style.css"
+    }
+}
+
+
+
+function trans () {
+    let translate = document.getElementById("translate");
+    let nameIs = document.getElementById("nameIs");
+    let name = document.getElementById("name")
+    let profession = document.getElementById("profession")
+    let home = document.getElementById("home")
+    let aboutM = document.getElementById("aboutM") 
+    let project = document.getElementById("project")
+    if (1===1) {
+        translate.innerHTML = "Я занимаюсь версткой сайтов! Занимаюсь этим больше года! Могу создавать многостраничные сайты с некоторым функционалом."
+        nameIs.innerHTML = "Меня зовут"
+        name.innerHTML = "Егор Паньков и я "
+        profession.innerHTML = " начинающий веб - разработчик"
+        home.innerHTML = "Главная"
+        aboutM.innerHTML = "Обо мне"
+        project.innerHTML = "Проект"
+    } else {
+        console.log('krah')
+    }
+}
+
+
+
+function transs () {
+    let about = document.getElementById("about")
+    let project = document.getElementById("project")
+    let home = document.getElementById("home")
+    let aboutM = document.getElementById("aboutM") 
+    let names = document.getElementById("names")
+    let fam = document.getElementById("fam")
+    let pro = document.getElementById("pro")
+    let prof = document.getElementById("prof")
+    let pers = document.getElementById("pers")
+    let cre = document.getElementById("cre")
+    let problem = document.getElementById("problem")
+    let design = document.getElementById("design")
+    let analytical = document.getElementById("analytical")
+    let debug = document.getElementById("debug")
+    let persistence = document.getElementById("persistence")
+    let control = document.getElementById("control")
+    let ability = document.getElementById("ability")
+    let correct = document.getElementById("correct")
+    let good = document.getElementById("good")
+    let test = document.getElementById("test")
+    let initiative = document.getElementById("initiative")
+    let maintain = document.getElementById("maintain")
+    let effort = document.getElementById("effort")
+    let create = document.getElementById("create")
+    let algoritm = document.getElementById("algoritm")
+    let optimize = document.getElementById("optimize")
+    let respons = document.getElementById("respons")
+    let maintaining = document.getElementById("maintaining")
+    let commun = document.getElementById("commun")
+
+    if (1===1) {
+        home.innerHTML = "Главная"
+        aboutM.innerHTML = "Обо мне"
+        project.innerHTML = "Проект"
+        about.innerHTML = "Мне 16 лет и я до сих пор хожу в школу, на одном из моих занятий по информатике мы обсуждали тему создания сайтов, мне было очень интересно, а на следующий день я нашел в интернете много литературы и видеокурсов, так я стал верстальщиком сайтов! В свободное время играю с друзьями в футбол и баскетбол, посещаю секцию самбо. Этот сайт для меня является проектом, с помощью которого я смогу себе ответить на вопрос: Смогу ли я в будущем стать веб - программистом?"
+        names.innerHTML = "Егор"
+        fam.innerHTML = "Паньков"
+        pro.innerHTML = "Профессиональные навыки и личностные качества"
+        prof.innerHTML = "Профессиональные навыки"
+        pers.innerHTML = "Личностные качества"
+        cre.innerHTML = "Создавать веб-сайты"
+        problem.innerHTML = "Умение решать задачи"
+        design.innerHTML = "Разработка дизайна веб-ресурса"
+        analytical.innerHTML = "Аналитический склад ума"
+        debug.innerHTML = "Заниматься отладкой и тестированием сайта"
+        persistence.innerHTML = "Упорство"
+        control.innerHTML = "Контролировать работу веб-ресурса"
+        ability.innerHTML = "Умение работать в команде"
+        correct.innerHTML = "Вовремя исправлять ошибки, возникающие у пользователей"
+        good.innerHTML = "Хорошая концентрация"
+        test.innerHTML = "Тестировать сайт на надежность и функциональность"
+        initiative.innerHTML = "Инициативность"
+        maintain.innerHTML = "Поддерживать ресурс в рабочем состоянии"
+        effort.innerHTML = "Усидчивость"
+        create.innerHTML = "Создавать техническую, пользовательскую и эксплуатационную документацию"
+        algoritm.innerHTML = "Алгоритмизированный подход"
+        optimize.innerHTML = "Оптимизировать веб-сайт под мобильные условия"
+        respons.innerHTML = "Ответственность"
+        maintaining.innerHTML = "Ведение контроля за соблюдением авторских, смежных прав, рекламы и пр."
+        commun.innerHTML = "Коммуникабельность"
+    } else {
+        console.log('krah')
+    }
+}
+function transss () {
+    let home = document.getElementById("home")
+    let aboutM = document.getElementById("aboutM") 
+    let project = document.getElementById("project")
+    let proj = document.getElementById("proj")
+    let rele = document.getElementById("rele")
+    let the = document.getElementById("the")
+    let prob = document.getElementById("prob")
+    let atten = document.getElementById("atten")
+    let hypo = document.getElementById("hypo")
+    let i = document.getElementById("i")
+    let targ = document.getElementById("targ")
+    let conf = document.getElementById("conf")
+    let task = document.getElementById("task")
+    let study = document.getElementById("study")
+    let learn = document.getElementById("learn")
+    let fam = document.getElementById("fam")
+    let choo = document.getElementById("choo")
+    let develop = document.getElementById("develop")
+    let cond = document.getElementById("cond")
+    let pub = document.getElementById("pub")
+    let concl = document.getElementById("concl")
+    let res = document.getElementById("res")
+    let confirmation = document.getElementById("confirmation")
+    let work = document.getElementById("work")
+    let clicker = document.getElementById("clicker")
+    let gallows = document.getElementById("gallows")
+    
+
+
+    if (1===1) {
+        home.innerHTML = "Главная"
+        aboutM.innerHTML = "Обо мне"
+        project.innerHTML = "Проект"
+        proj.innerHTML = "Описание проекта"
+        rele.innerHTML = "Актуальность"
+        the.innerHTML = "Профессия Web - разработчика для меня актуальна, так как в будущем я хочу стать профессиональным Web - разработчиком. Данный проект для меня важен, так как он позволяет мне подтвердить или опровергнуть мой выбор индивидуальной образовательной траектории"
+        prob.innerHTML = "Проблема"
+        atten.innerHTML = "Посещая уроки информатики в основной школе и курс по выбору “Web-дизайн и основы сайтостроения” мне понравилась тема “Введение в программирование”. Я задался вопросом смогу ли я стать программистом, а именно Web-программистом, создавать Web-сайты. Я решил попробовать себя в роли Web - разработчика сайта, с целью подтверждения или опровержения моего выбора ИОТ"
+        hypo.innerHTML = "Гипотеза"
+        i.innerHTML = "Я предполагаю что сделал правильный выбор ИОТ и смогу стать профессиональным Web-разработчиком"
+        targ.innerHTML = "Цель"
+        conf.innerHTML = "Подтверждение правильности выбора ИОТ, через разработку Web – сайта и размещение его в сети Интернет"
+        task.innerHTML = "Задачи"
+        study.innerHTML = "Изучение профессиональных обязанностей и личностных качеств Web-разработчика"
+        learn.innerHTML = "Ознакомиться с технологиями создания сайтов"
+        fam.innerHTML = "Ознакомиться с основными языками программирования и начать их изучение"
+        choo.innerHTML = "Выбрать платформу для разработки сайта"
+        develop.innerHTML = "Разработать макета сайта"
+        cond.innerHTML = "Провести тестирование и отладку сайта"
+        pub.innerHTML = "Опубликовать сайт в сети Интернет"
+        concl.innerHTML = "Выводы"
+        res.innerHTML = "Результат"
+        confirmation.innerHTML = "Подтверждение или опровержение гипотезы"
+        work.innerHTML = "Мои работы:"
+        clicker.innerHTML = "Кликер"
+        gallows.innerHTML = "Виселица"
+
+
+        
+    } else {
+        console.log('krah')
+    }
+}
